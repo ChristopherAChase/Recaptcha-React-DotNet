36b1d764a8bb14f81f9b5be09a0d42ab0c70286b
Zach Robinson <59841145+zprobinson@users.noreply.github.com>
2023-03-06 09:24:18 -0600

    Update IRecaptchaRequestMessage.cs

--- RecaptchaValidation/Interfaces/IRecaptchaRequestMessage.cs
+++ RecaptchaValidation/Interfaces/IRecaptchaRequestMessage.cs
@@ -1,11 +1,12 @@
 ﻿namespace RecaptchaValidation.Interfaces
 {
+    // You don't need an interface for a data transfer object (DTO). 
+    // I can imagine no other possible implementations of a simple object that can hold 4 strings. -z
     public interface IRecaptchaRequestMessage
     {
         string path { get; }
         string secret { get; }
         string response { get; set; }
         string remoteip { get; set; }
-
     }
 }
