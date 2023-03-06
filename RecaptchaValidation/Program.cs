e3ce685196100dbcaeece9d55e56c73e223ebdec
Zach Robinson <59841145+zprobinson@users.noreply.github.com>
2023-03-06 09:12:23 -0600

    Update Program.cs
    Use const instead of var for hard coded string variables.
    I don't know if both of the registrations are required.

--- RecaptchaValidation/Program.cs
+++ RecaptchaValidation/Program.cs
@@ -3,7 +3,7 @@ using RecaptchaValidation.Models;
 using RecaptchaValidation.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-var AllowAllOriginsCorsPolicy = "_AllowAllOriginsCorsPolicy";
+const string AllowAllOriginsCorsPolicy = "_AllowAllOriginsCorsPolicy";
 // Add services to the container.
 builder.Services.AddCors(options =>
 {
@@ -17,8 +17,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// I'm not sure why we are both creating an HTTP Client AND a transient service, probably only need one. -z
 builder.Services.AddHttpClient<IRecaptchaService, RecaptchaService>();
 builder.Services.AddTransient<IRecaptchaService, RecaptchaService>();
+
 builder.Services.Configure<RecaptchaOptions>(
     builder.Configuration.GetSection(RecaptchaOptions.RecaptchaV3));
 
