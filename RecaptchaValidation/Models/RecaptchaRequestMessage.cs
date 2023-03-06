285a7734751f706dd263e0c9038d16bd616638c1
Zach Robinson <59841145+zprobinson@users.noreply.github.com>
2023-03-06 09:46:54 -0600

    Update RecaptchaRequestMessage.cs

--- RecaptchaValidation/Models/RecaptchaRequestMessage.cs
+++ RecaptchaValidation/Models/RecaptchaRequestMessage.cs
@@ -4,8 +4,12 @@ using System.Web;
 
 namespace RecaptchaValidation.Models
 {
-    public class RecaptchaRequestMessage : IRecaptchaRequestMessage
+    // This file isn't being used outside of the assembly. It is also something you don't want people inheriting from.
+    // Use _internal_ to protect this message from being utilized by other projects.
+    // Use _sealed_ to protect from the awfulness which is inheritance.
+    internal sealed class RecaptchaRequestMessage : IRecaptchaRequestMessage
     {
+        // Traditionally, properties are PascalCased and fields are _camelCased.
         public string path { get; private set; }
         public string secret { get; private set; }
         public string response { get; set; }
@@ -19,6 +23,11 @@ namespace RecaptchaValidation.Models
             remoteip = _remoteIp;
         }
 
+        // ToString() is typically used for getting a representation of a C# object.
+        // I can see the use here, but I would prefer a utility function that is more descriptive.
+        // public string GetVerificationUrl().
+        // Or even better, a pure function:
+        // public static string GetVerificationUrl(RecaptchaRequestMessage message) => {};
         public override string ToString()
         {
             return $"{path}?{HttpUtility.UrlPathEncode($"secret={secret}&response={response}&remoteip={remoteip}")}";
