1460eab51c0bcc43c57527d8f67ef56b230e8653
Zach Robinson <59841145+zprobinson@users.noreply.github.com>
2023-03-06 09:24:57 -0600

    Update IRecaptchaResponseMessage.cs

--- RecaptchaValidation/Interfaces/IRecaptchaResponseMessage.cs
+++ RecaptchaValidation/Interfaces/IRecaptchaResponseMessage.cs
@@ -2,6 +2,7 @@
 
 namespace RecaptchaValidation.Interfaces
 {
+    // You don't need an interface for a data transfer object (DTO). -z
     public interface IRecaptchaResponseMessage
     {
         bool success { get; set; }
